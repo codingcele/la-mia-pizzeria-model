6e05b6416c4641e3669800d37cac5df6f72a252f
codingcele <marcocelegato9@gmail.com>
2023-05-09 10:52:11 +0200

    MVC pizze

---
+++ la-mia-pizzeria-static/Controllers/PizzeriaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static
+{
+    public class PizzeriaController : Controller
+    {
+        public readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PizzeriaController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            var pizze = new List<Pizza>
+            {
+                new Pizza
+                {
+                    Image =  "margherita.jpg",
+                    Name = "Margherita",
+                    Ingredients = "pomodoro, mozzarella, basilico fresco, sale, olio.",
+                    Price = 5.00m
+                },
+                new Pizza
+                {
+                    Image = "prosciutto-e-funghi.jpg",
+                    Name = "Prosciutto e funghi",
+                    Ingredients = "pomodoro, mozzarella, prosciutto cotto, funghi champignon, sale, olio.",
+                    Price = 6.50m
+                },
+                new Pizza
+                {
+                    Image = "viennese.jpg",
+                    Name = "Viennese",
+                    Ingredients = "pomodoro, mozzarella, wurstel, sale, olio.",
+                    Price = 6.00m
+                },
+                new Pizza
+                {
+                    Image = "diavola.jpg",
+                    Name = "Salamino piccante",
+                    Ingredients = "pomodoro, mozzarella, salamino piccante, sale, olio.",
+                    Price = 6.50m
+                },
+            };
+
+            return View(pizze);
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public decimal Price { get; set; }
+    }
+}
