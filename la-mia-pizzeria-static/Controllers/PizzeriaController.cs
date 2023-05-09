bc04cb44cf2d12a0549fb2252230db510d17455a
codingcele <marcocelegato9@gmail.com>
2023-05-09 11:15:42 +0200

    gestione menù vuoto

--- la-mia-pizzeria-static/Controllers/PizzeriaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzeriaController.cs
@@ -14,34 +14,34 @@ namespace la_mia_pizzeria_static
         {
             var pizze = new List<Pizza>
             {
-                new Pizza
-                {
-                    Image =  "margherita.jpg",
-                    Name = "Margherita",
-                    Ingredients = "pomodoro, mozzarella, basilico fresco, sale, olio.",
-                    Price = 5.00m
-                },
-                new Pizza
-                {
-                    Image = "prosciutto-e-funghi.jpg",
-                    Name = "Prosciutto e funghi",
-                    Ingredients = "pomodoro, mozzarella, prosciutto cotto, funghi champignon, sale, olio.",
-                    Price = 6.50m
-                },
-                new Pizza
-                {
-                    Image = "viennese.jpg",
-                    Name = "Viennese",
-                    Ingredients = "pomodoro, mozzarella, wurstel, sale, olio.",
-                    Price = 6.00m
-                },
-                new Pizza
-                {
-                    Image = "diavola.jpg",
-                    Name = "Salamino piccante",
-                    Ingredients = "pomodoro, mozzarella, salamino piccante, sale, olio.",
-                    Price = 6.50m
-                },
+                //new Pizza
+                //{
+                //    Image =  "margherita.jpg",
+                //    Name = "Margherita",
+                //    Ingredients = "pomodoro, mozzarella, basilico fresco, sale, olio.",
+                //    Price = 5.00m
+                //},
+                //new Pizza
+                //{
+                //    Image = "prosciutto-e-funghi.jpg",
+                //    Name = "Prosciutto e funghi",
+                //    Ingredients = "pomodoro, mozzarella, prosciutto cotto, funghi champignon, sale, olio.",
+                //    Price = 6.50m
+                //},
+                //new Pizza
+                //{
+                //    Image = "viennese.jpg",
+                //    Name = "Viennese",
+                //    Ingredients = "pomodoro, mozzarella, wurstel, sale, olio.",
+                //    Price = 6.00m
+                //},
+                //new Pizza
+                //{
+                //    Image = "diavola.jpg",
+                //    Name = "Salamino piccante",
+                //    Ingredients = "pomodoro, mozzarella, salamino piccante, sale, olio.",
+                //    Price = 6.50m
+                //},
             };
 
             return View(pizze);
