37d5d35b4845ff53bf4d6a73449539eaa0ab0e1c
codingcele <marcocelegato9@gmail.com>
2023-05-10 12:02:43 +0200

    dati database passati alla view dal controller in forma di model

--- la-mia-pizzeria-static/Controllers/PizzeriaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzeriaController.cs
@@ -1,50 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace la_mia_pizzeria_static
 {
     public class PizzeriaController : Controller
     {
-        public readonly IWebHostEnvironment _webHostEnvironment;
-
-        public PizzeriaController(IWebHostEnvironment webHostEnvironment)
-        {
-            _webHostEnvironment = webHostEnvironment;
-        }
         public IActionResult Index()
         {
-            var pizze = new List<Pizza>
+            using (PizzeriaContext db = new PizzeriaContext())
             {
-                //new Pizza
-                //{
-                //    Image =  "margherita.jpg",
-                //    Name = "Margherita",
-                //    Ingredients = "pomodoro, mozzarella, basilico fresco, sale, olio.",
-                //    Price = 5.00m
-                //},
-                //new Pizza
-                //{
-                //    Image = "prosciutto-e-funghi.jpg",
-                //    Name = "Prosciutto e funghi",
-                //    Ingredients = "pomodoro, mozzarella, prosciutto cotto, funghi champignon, sale, olio.",
-                //    Price = 6.50m
-                //},
-                //new Pizza
-                //{
-                //    Image = "viennese.jpg",
-                //    Name = "Viennese",
-                //    Ingredients = "pomodoro, mozzarella, wurstel, sale, olio.",
-                //    Price = 6.00m
-                //},
-                //new Pizza
-                //{
-                //    Image = "diavola.jpg",
-                //    Name = "Salamino piccante",
-                //    Ingredients = "pomodoro, mozzarella, salamino piccante, sale, olio.",
-                //    Price = 6.50m
-                //},
-            };
+                List<Pizza> pizze = db.Pizze.ToList();
 
-            return View(pizze);
+                return View(pizze);
+            }
+           
         }
+
+        //public IActionResult Details(int id)
+        //{
+        //    using (PizzeriaContext context = new PizzeriaContext())
+        //    {
+        //        var pizzaId = context.Pizze.FirstOrDefault(p => p.Id == id);
+                
+        //        return View("Details", pizzaId);
+        //    }
+        //}
+
     }
 }
